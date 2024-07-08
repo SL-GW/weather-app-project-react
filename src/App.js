548194b80693320fbb5eb1b34ac5be236e1d4f46
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-07-08 15:26:29 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <footer>
-        This project was created by Sarah Gray. It is open-sourced on{" "}
-        <a
-          href="https://github.com/SL-GW/weather-app-project-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-        and is hosted on{" "}
-        <a
-          href="https://weather-app-project-with-react.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather Search Engine</h1>
+
+        <footer>
+          This project was created by Sarah Gray. It is open-sourced on{" "}
+          <a
+            href="https://github.com/SL-GW/weather-app-project-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and is hosted on{" "}
+          <a
+            href="https://weather-app-project-with-react.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
