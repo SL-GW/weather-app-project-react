743fcf1f07cacf2b64e686c4b05897a70268c1b3
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-07-08 14:11:37 +0200

    Added footer with links to Github and Netlify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ function App() {
   return (
     <div className="App">
       <h1>Weather Search Engine</h1>
+      <footer>
+        This project was created by Sarah Gray. It is open-sourced on{" "}
+        <a
+          href="https://github.com/SL-GW/weather-app-project-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and is hosted on{" "}
+        <a
+          href="https://weather-app-project-with-react.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </footer>
     </div>
   );
 }
