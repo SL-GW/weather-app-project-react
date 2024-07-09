a630303f7e6ee81d7d168cb3c6a26cd451b112bd
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-07-09 16:41:55 +0200

    Added date and tim e on weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 
 export default function Weather(props) {
   const [ready, setReady] = useState(false);
@@ -16,7 +17,7 @@ export default function Weather(props) {
       feels: response.data.temperature.feels_like,
       description: response.data.condition.description,
       iconUrl: response.data.condition.icon_url,
-      date: "Tuesday 12:49",
+      date: new Date(response.data.time * 1000),
     });
 
     setReady(true);
@@ -36,7 +37,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-4">
